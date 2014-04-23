d6d44ebb573972e4653a920b4e8c23034a033b77
Keavon Chambers <keavon@keavon.com>
2014-04-23 16:31:12 -0700

    Finished circle capturing and restructured user input code to prepare for multiplayer integration.

--- client/update.js
+++ client/update.js
@@ -1,4 +1,6 @@
-﻿var canvas = document.getElementById("game");
+﻿/*JSCS "requireKeywordsOnNewLine": ["else"] */
+
+var canvas = document.getElementById("game");
 var context = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -13,7 +15,8 @@ function requestFullScreen(element)
 	if (requestMethod)
 	{
 		requestMethod.call(element);
-	} else if (typeof window.ActiveXObject !== "undefined")
+	}
+	else if (typeof window.ActiveXObject !== "undefined")
 	{
 		var wscript = new ActiveXObject("WScript.Shell");
 		if (wscript !== null)
@@ -37,32 +40,32 @@ var players = {
 	"players": [
 		{
 			"ID": 1397946725450, // Example value, normally set by server as the number of milliseconds since epoch when client connects
-			"x": 0,
-			"y": 0,
-			"lastX": 0,
-			"lastY": 0,
+			"x": 0.0,
+			"y": 0.0,
 			"residence": 0,
-			"angle": 0,
-			"velocity": 0,
-			"angularVelocity": 0,
+			"angle": 0.0,
+			"speed": 25.0,
+			"currentSpeed": 0,
+			"justJumped": false,
+			"velocity": 0.0,
+			"angularVelocity": 0.0,
 			"color": "#ff0000",
-			"speed": 25,
 			"jumpVelocity": 0.5,
-			"size": 20
+			"size": 20.0
 		}, {
 			"ID": 1397946730842, // Example value, normally set by server as the number of milliseconds since epoch when client connects
-			"x": 0,
-			"y": 0,
-			"lastX": 0,
-			"lastY": 0,
+			"x": 0.0,
+			"y": 0.0,
 			"residence": 2,
-			"angle": 0,
-			"velocity": 0,
-			"angularVelocity": 0,
+			"angle": 0.0,
+			"speed": 25.0,
+			"currentSpeed": 0,
+			"justJumped": false,
+			"velocity": 0.0,
+			"angularVelocity": 0.0,
 			"color": "#00ffff",
-			"speed": 25,
 			"jumpVelocity": 0.5,
-			"size": 20
+			"size": 20.0
 		}
 	]
 };
@@ -102,12 +105,14 @@ function deltaTime()
 	return dt;
 }
 
-context.font = "72px Arial";
+context.font = "72px Open Sans, Arial";
 context.textAlign = 'center';
 context.fillText("Connecting to server...", canvas.offsetWidth * 0.5, canvas.offsetHeight * 0.5);
 context.fill();
 
 (function () { var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame; window.requestAnimationFrame = requestAnimationFrame; })();
+
+var userIdElement = -1;
 
 function updateFrame()
 {
@@ -117,6 +122,28 @@ function updateFrame()
 	// Clear frame
 	context.clearRect(0, 0, canvas.width, canvas.height);
 
+	// Find index of the user --- RECALCULATE WHEN PLAYER LEAVES
+	if (userIdElement == -1)
+	{
+		for (var playerId = 0; playerId < players.players.length; playerId++)
+		{
+			if (players.players[playerId].ID == userID)
+			{
+				userIdElement = playerId;
+				break;
+			}
+		}
+	}
+
+	// Check if user jumps from input
+	if (checkForJump())
+	{
+		players.players[userIdElement].justJumped = true;
+	}
+
+	// Get user movement direction from input
+	players.players[userIdElement].currentSpeed = keyHandler(userIdElement);
+
 	// Updates player locations
 	updatePlayers();
 
@@ -124,6 +151,7 @@ function updateFrame()
 	var offsetX = players.players[userIdElement].x - (canvas.offsetWidth * 0.5);
 	var offsetY = players.players[userIdElement].y - (canvas.offsetHeight * 0.5);
 
+	// Update world circle capture data
 	updateCircles();
 
 	// Draws world circles
@@ -141,16 +169,21 @@ function updateFrame()
 			var color = "";
 			if (i < circles.circles[worldCircles].capture.length)
 			{
+				// Calculate claimed parts
 				nextSize -= circles.circles[worldCircles].capture[i].percentage;
 				color = players.players.find(function (x) { return x.ID === circles.circles[worldCircles].capture[i].id; }).color; // jshint ignore:line
-			} else if (nextSize > 0)
+			}
+			else if (nextSize > 0)
 			{
+				// Calculate unclaimed part
 				color = "#cccccc";
-			} else
+			}
+			else
 			{
 				break;
 			}
 
+			// Draw
 			context.beginPath();
 			context.fillStyle = color;
 			context.arc(centerX, centerY, circleR * size, 0, 2 * Math.PI);
@@ -160,7 +193,10 @@ function updateFrame()
 			{
 				break;
 			}
-			size = nextSize;
+			else
+			{
+				size = nextSize;
+			}
 		}
 	}
 
@@ -181,35 +217,34 @@ function updatePlayers()
 {
 	for (var playerId = 0; playerId < players.players.length; playerId++)
 	{
-		var movementDirection = 0;
 		if (players.players[playerId].ID == userID)
 		{
 			// Checks if the user has jumped
-			movementDirection = keyHandler(playerId);
-			userIdElement = playerId;
-
-			if (checkForJump())
+			if (players.players[playerId].justJumped)
 			{
-				console.log("test");
-				for (var captives = 0; captives < circles.circles[players.players[playerId].residence].capture.length; captives++)
+				// Remove "active" tag from former circle
+				for (var captives = 0; captives < circles.circles[players.players[playerId].residence].capture.length; captives++) // Look through each player capturing circle
 				{
 					if (circles.circles[players.players[playerId].residence].capture[captives].id == players.players[playerId].ID)
 					{
 						circles.circles[players.players[playerId].residence].capture[captives].active = false;
+						break;
 					}
 				}
+
+				// Remove player from circle
 				players.players[playerId].residence = null;
 				players.players[playerId].velocity = players.players[playerId].jumpVelocity;
 				players.players[playerId].angularVelocity = players.players[playerId].angle;
+				players.players[playerId].justJumped = false;
 			}
-
 		}
 
 		// Move around edge of circle
 		if (players.players[playerId].residence !== null)
 		{
 			// Modifies the angle
-			players.players[playerId].angle += (players.players[playerId].speed * movementDirection * deltaT) / circles.circles[players.players[playerId].residence].r;
+			players.players[playerId].angle += (players.players[playerId].speed * players.players[playerId].currentSpeed * deltaT) / circles.circles[players.players[playerId].residence].r;
 
 			// Turn that angle into XY coordinates and update the player object
 			var angle = players.players[playerId].angle;
@@ -238,11 +273,13 @@ function updatePlayers()
 					"percentage": 0.0,
 					"active": true
 				});
-			} else
+			}
+			else
 			{
 				circles.circles[players.players[playerId].residence].capture[i].active = true;
 			}
-		} else // Move when player is flying
+		}
+		else // Move when player is flying
 		{
 			// Calculate position for this frame during flight
 			players.players[playerId].x += players.players[playerId].velocity * Math.cos(players.players[playerId].angularVelocity * degToRad) * deltaT;
@@ -250,10 +287,6 @@ function updatePlayers()
 
 			for (var testNumber = 0; testNumber < circles.circles.length; testNumber++)
 			{
-				// Checks if the player is within the X and Y viscinity of the tested circle
-				//if (players.players[playerId].x > (circles.circles[testNumber].r + players.players[playerId].size) - circles.circles[testNumber].x && players.players[playerId].x < (circles.circles[testNumber].r + players.players[playerId].size) + circles.circles[testNumber].x && players.players[playerId].y > (circles.circles[testNumber].r + players.players[playerId].size) - circles.circles[testNumber].y && players.players[playerId].y < (circles.circles[testNumber].r + players.players[playerId].size) + circles.circles[testNumber].y)
-				//{
-
 				// Checks if the player is within the radius of the player + tested circle to determine if they're touching
 				if (Math.sqrt(Math.pow(players.players[playerId].x - circles.circles[testNumber].x, 2) + Math.pow(players.players[playerId].y - circles.circles[testNumber].y, 2)) <= circles.circles[testNumber].r + players.players[playerId].size)
 				{
@@ -263,8 +296,6 @@ function updatePlayers()
 					//var collisionRadius = circles.circles[playerId].r + players.players[playerId].size;
 					players.players[playerId].angle = Math.atan2(players.players[playerId].y - circles.circles[players.players[playerId].residence].y, players.players[playerId].x - circles.circles[players.players[playerId].residence].x) * radToDeg;
 				}
-
-				//}
 			}
 		}
 	}
@@ -275,22 +306,34 @@ function updateCircles()
 	// Repeat for every circle
 	for (var checkedCircle = 0; checkedCircle < circles.circles.length; checkedCircle++)
 	{
+		var totalCaptured = 0.0;
+
+		for (var ii = 0; ii < circles.circles[checkedCircle].capture.length; ii++)
+		{
+			totalCaptured += circles.circles[checkedCircle].capture[ii].percentage;
+		}
+
 		// Repeat for every capturing player
 		for (var i = 0; i < circles.circles[checkedCircle].capture.length; i++)
 		{
-			if (circles.circles[checkedCircle].capture[i].active)
+			// If it's active, grow circle
+			if (circles.circles[checkedCircle].capture[i].active && totalCaptured < 1)
 			{
 				circles.circles[checkedCircle].capture[i].percentage += deltaT / circles.circles[checkedCircle].r * 0.05;
 				if (circles.circles[checkedCircle].capture[i].percentage > 1)
 				{
 					circles.circles[checkedCircle].capture[i].percentage = 1;
 				}
-			} else
+			}
+			// If it's not active, shrink circle
+			else if (!circles.circles[checkedCircle].capture[i].active)
 			{
 				circles.circles[checkedCircle].capture[i].percentage -= deltaT / circles.circles[checkedCircle].r * 0.01;
+
 				if (circles.circles[checkedCircle].capture[i].percentage < 0)
 				{
-					circles.circles[checkedCircle].capture[i].percentage = 0;
+					// Remove old capturer
+					circles.circles[checkedCircle].capture.splice(i, 1);
 				}
 			}
 		}
